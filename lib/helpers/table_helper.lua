c6d0f72fa6832b92033edd40f61b8962c0099da6
Mitch LeBlanc <supermitch@gmail.com>
2013-03-21 17:07:41 -0700

    Add comment

--- lib/helpers/table_helper.lua
+++ lib/helpers/table_helper.lua
@@ -2,6 +2,7 @@ module(..., package.seeall)
 
 function is_in(value, table)
     -- Return true if value is in table values
+    -- Similar to Python's "in" keyword, e.g. 'horse' in ['the horse'] == True
     for _, v in ipairs(table) do
         if v == value then
             return true
